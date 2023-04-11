ddf48a55aa604e72ea934198171bb47cc8dbd310
luis <lfilipe780@gmail.com>
2023-04-11 00:38:09 -0300

    Finalização da cena CARTA e adiconando background no Menu inicial

---
+++ scripts/carta.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+
+var carta : Card
+var filiacao : String
+var especial : Card
+
+#
+
+func _ready():
+	#Especifico para teste
+	###
+	#randomize()
+	#var indice_aleatorio = randi() % CardsController.listaStroj.size()
+	#print(indice_aleatorio)
+	#indice_aleatorio = 3 #Especifico para teste
+	#filiacao = "Stroj"
+	#carta = CardsController.listaStroj[indice_aleatorio]
+	#$CardSprite.texture = carta.textura
+	###
+	if carta.EspecialID > 0:
+		if filiacao.to_upper() == "STROJ":
+			especial = CardsController.buscarID(carta.EspecialID,CardsController.listaStrojEspecial)
+		else:
+			especial = CardsController.buscarID(carta.EspecialID,CardsController.listaGaiaEspecial)
+		$VerEspecialButton.visible = true
+		
+
+func inicializar(index:int,filiacao:String):
+	#Seta a filiação da carta
+	self.filiacao = filiacao
+	#Carrega a carta do Controlador
+	carta = CardsController.listaStroj[index]
+	$CardSprite.texture = carta.textura
+	#Checa se tem carta especial relacionada e faz o tratamento
+	if carta.EspecialID > 0:
+		#Se tiver, é feita a checagem de filiacao | carrega os dados e torna botao de card especial visivel
+		if filiacao.to_upper() == "STROJ":
+			especial = CardsController.buscarID(carta.EspecialID,CardsController.listaStrojEspecial)
+		else:
+			especial = CardsController.buscarID(carta.EspecialID,CardsController.listaGaiaEspecial)
+		$VerEspecialButton.visible = true
+
+#Funcao ativada quando o botão de ver card especial é pressionado
+func _on_VerEspecialButton_toggled(button_pressed):
+	#Se estiver pressionado mostra a carta especial relacionada, se não mostra a carta normal
+	if $VerEspecialButton.pressed:
+		$CardSprite.texture = especial.textura
+	else:
+		$CardSprite.texture = carta.textura
---
+++ scripts/controllers/cardsController.gd
@@ -0,0 +1,48 @@
+extends Node
+
+#Variaveis da Galaxia de Gaia
+var listaGaia := [] 
+var listaGaiaEspecial := []
+
+#Variaveis da Galaxia de Stroj
+var listaStroj := [] 
+var listaStrojEspecial := []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#Fazendo a leitura das cartas a partir dos Resources de cada uma delas
+	listaGaia = loadItems("res://resources/gaia/amazonas/normal/")
+	listaGaiaEspecial = loadItems("res://resources/gaia/amazonas/especial/")
+	listaStroj = loadItems("res://resources/stroj/goldrons/normal/")
+	listaStrojEspecial = loadItems("res://resources/stroj/goldrons/especial/")
+	
+static func loadItems(folderPath : String) -> Array:
+	var item_files := []
+	var items_folder := folderPath#"res://resources/gaia/amazonas/"
+
+	var directory := Directory.new()
+	var can_continue := directory.open(items_folder) == OK
+	if not can_continue:
+		print_debug('Could not open directory "%s"' % [items_folder])
+		return item_files
+
+	directory.list_dir_begin(true, true)
+	var file_name := directory.get_next()
+	while file_name != "":
+		if file_name.get_extension() == "tres":
+			item_files.append(items_folder.plus_file(file_name))
+		file_name = directory.get_next()
+
+	var item_resources := []
+	for path in item_files:
+		item_resources.append(load(path))
+
+	return item_resources
+	
+func buscarID(id:int,lista) -> Card:
+	for elemento in lista:
+		print("Elmento ",elemento.ID," checado!")
+		if elemento.ID == id:
+			return elemento
+	
+	return null
