fa91c1409f918d3287c160afcb4025ee3afe97fa
Luís Filipe Santos Seixas <lfilipe780@gmail.com>
2023-06-01 21:11:55 -0300

    Renomeando nome de efeitos que estavam errados

---
+++ resources/fortaleza.gd
@@ -0,0 +1,11 @@
+class_name Fortaleza
+extends Resource
+
+
+export var ID : String
+export var Nome : String
+export(PoolStringArray) var Efeitos := []
+export(PoolStringArray) var TipoEfeitos := []
+export(PoolIntArray) var CustoEnergia := []
+export var Vida : int
+export var textura : Texture
